36aa28a6ac3dc6c61b2e5ba48b37c568ea34ed98
saadniazi13 <saadniazi729@gmail.com>
2026-06-30 17:32:30 +0500

    Update bot settings for name and owner

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Graphics Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Graphics Bot",
+  botOwner: 'Saad Niazi', // Your name
+  ownerNumber: '+923095492394', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
